2e30d9352074a44f29f32a619a53c7275876bd6a
Furiduri <jorge.furiduri@gmail.com>
2019-05-17 18:12:30 -0500

    test Firebase 1

--- app/src/main/java/com/example/app_control_domotico/MainActivity.java
+++ app/src/main/java/com/example/app_control_domotico/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.app_control_domotico;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.*;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    protected FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+
+    }
+
+    public void OnClic(View  view){
+        if(view.getId() == R.id.btnLed1){
+            FirebaseApp app = db.getApp();
+            Toast.makeText(getApplicationContext(),document.getId() + " => " + document.getData(),Toast.LENGTH_SHORT).show();
+        }
     }
 }
